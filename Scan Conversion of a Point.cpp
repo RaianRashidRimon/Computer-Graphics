f78cc05d0c356466fde1fa46aec934ef59245acc
RaianRashidRimon <rimonraian2@gmail.com>
2025-02-08 20:02:23 +0600

    scanning a point

---
+++ Scan Conversion of a Point.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float x,y;
+    cout << "Enter point(x,y)" << endl;
+    cin >> x>>y;
+    int px,py;
+    px = floor(x);
+    py = floor(y);
+    cout << "Scanned points are ("<<px<<","<<py<<")"<<endl;
+}
