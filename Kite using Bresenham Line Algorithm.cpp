a39f8fe836148c47e2f1520dc575bd149f0d50b8
Raian Rashid <rimonraian2@gmail.com>
2025-02-13 00:59:42 +0600

    kite

--- Kite using Bresenham Line Algorithm.cpp
+++ Kite using Bresenham Line Algorithm.cpp
@@ -8,12 +8,10 @@ void bresenhamLine(int x1, int y1, int x2, int y2, int color) {
     int sx = (x1 < x2) ? 1 : -1;
     int sy = (y1 < y2) ? 1 : -1;
     int err = dx - dy;
-
     while (1) {
         putpixel(x1, y1, color);
         if (x1 == x2 && y1 == y2)
             break;
-
         int err2 = 2 * err;
         if (err2 > -dy) {
             err -= dy;
@@ -25,27 +23,20 @@ void bresenhamLine(int x1, int y1, int x2, int y2, int color) {
         }
     }
 }
-
 int main() {
     int gd = DETECT, gm;
     initwindow(1000, 600);
-
-    // Define kite coordinates
     int xa = 600, ya = 150;
     int xb = 400, yb = 50;
     int xc = 200, yc = 150;
     int xd = 300, yd = 300;
     int xe = 500, ye = 300;
-
-    // Draw the kite using Bresenham's Line Algorithm
     bresenhamLine(xb, yb, xa, ya, RED);
     bresenhamLine(xa, ya, xd, yd, RED);
     bresenhamLine(xb, yb, xc, yc, RED);
     bresenhamLine(xc, yc, xe, ye, RED);
     bresenhamLine(xd, yd, xe, ye, RED);
     bresenhamLine(xc, yc, xa, ya, RED);
-
-
     getch();
     closegraph();
     return 0;
